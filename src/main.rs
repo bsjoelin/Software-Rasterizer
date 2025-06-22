8215a97e56973af3971524a36cc05c1ba3d2825e
Benjamin H. Sjølin <benjaminsjolin@hotmail.dk>
2025-06-22 22:41:06 +0200

    ADD: Generate random triangles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rand::{rng, rngs::ThreadRng, Rng};
 use std::{fs::write, io};
 
 mod rendering;
@@ -9,6 +10,45 @@ use crate::vector_math::triangle::*;
 
 fn main() {
     create_test_image();
+}
+
+fn setup_triangles(screen_width: usize, screen_height: usize) -> (Vec<Float2>, Vec<Float2>, Vec<Float3>) {
+    const TRIANGLE_COUNT: usize = 250;
+
+    let mut points = vec![Float2::zeros(); TRIANGLE_COUNT * 3];
+    let mut velocities = vec![Float2::zeros(); points.len()];
+    let mut triangle_colors = vec![Float3::zeros(); TRIANGLE_COUNT];
+
+    let center = Float2::new((screen_width / 2) as f64, (screen_height / 2) as f64);
+    let mut g = rng();
+
+    for p in points.iter_mut() {
+        let random_point = random_float2(&mut g, screen_width, screen_height);
+        let offset = (&random_point - &center) * 0.3;
+        *p = &center + &offset;
+    }
+
+    for i in (0..velocities.len()).step_by(3) {
+        let random_velocity = random_float2(&mut g, screen_width, screen_height);
+        let velocity = (&random_velocity - &center) * 0.5;
+        velocities[i + 0] = velocity.clone();
+        velocities[i + 1] = velocity.clone();
+        velocities[i + 2] = velocity;
+        triangle_colors[i / 3] = random_color(&mut g);
+    }
+    (points, velocities, triangle_colors)
+}
+
+fn random_float2(rng: &mut ThreadRng, width: usize, height: usize) -> Float2 {
+    Float2::new(rng.random_range(0..width) as f64, rng.random_range(0..height) as f64)
+}
+
+fn random_color(rng: &mut ThreadRng) -> Float3 {
+    Float3::new(
+        rng.random_range(0.0..1.0),
+        rng.random_range(0.0..1.0),
+        rng.random_range(0.0..1.0)
+    )
 }
 
 fn create_test_image() -> () {
